d60c2f22ce936a0bc411bf9baa9d0a1113cf9407
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-04-28 13:57:39 +0900

    h5ファイルの中身を一部確認するためのファイル

---
+++ openh5.py
@@ -0,0 +1,12 @@
+import numpy as np
+import h5py
+filename = 'output/tennis/dvs_h5.h5'
+
+with h5py.File(filename, "r") as f:
+    print(f.keys())
+    print(f["events"][()])
+    x = f["events"][()]
+with open('test.txt', 'w') as f:
+    for i in x:
+
+        f.writelines(str(i[0])) 
