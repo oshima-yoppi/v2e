efa03c41768b7a43cbfc148b5f0f3a051cd1f650
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-04-30 15:46:39 +0900

    正解デーt取り出せるようにする

--- openh5.py
+++ openh5.py
@@ -1,5 +1,9 @@
+from cProfile import label
+from tkinter.ttk import LabelFrame
 import numpy as np
 import h5py
+import torch
+import re
 filename = 'output/dvs0.h5'
 
 with h5py.File(filename, "r") as f:
@@ -7,7 +11,21 @@ with h5py.File(filename, "r") as f:
     print(f["events"][()])
     print(f['truth'][()])
     x = f["events"][()]
-with open('test.txt', 'w') as f:
-    for i in x:
+    lable = f['truth'][()]
+    print(lable)
+    lable = lable[1:-1]
+    lable = lable.split(',')
+    lablex = float(lable[0])
+    labley = float(lable[1])
+    lablez = float(lable[2])
+li = torch.zeros(3, 3)
+print(li)
+li[1, 0] = lablex
+li[1, 1] = labley
+li[1, 2] = lablez
+print(li.view(-1))
 
-        f.writelines(str(i[0])) 
+##テキストファイルに書き込み
+# with open('test.txt', 'w') as f:
+#     for i in x:
+#         f.writelines(str(i[0])) 
