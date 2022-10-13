ec67f4283ebc8966a1b933c09465b7b6d7d7ea50
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-10-13 15:41:06 +0900

    角速度ベクトルの学習用データを作成

--- cmd.py
+++ cmd.py
@@ -1,7 +1,7 @@
 import os 
 import glob
 from tqdm import tqdm
-dir = 'C:/Users/oosim/Desktop/avi/'
+dir = 'C:/Users/oosim/Desktop/avi3/'
 avi_data = []
 for _, _, files in os.walk(dir):
     for file in files:
@@ -9,8 +9,9 @@ for _, _, files in os.walk(dir):
 print(avi_data)
 for i, data in enumerate(tqdm(avi_data)):
     data_ = data.split('_')
-    x ,y,z = data_[1], data_[2],data_[3]
+    x ,y,z = data_[2], data_[3],data_[4]
     # string = "python v2e.py -i {}  --output_folder=output/ --dvs_text DVS_TEXT{} --dvs_h5 dvs{} --dvs_exposure duration 0.01 --overwrite --timestamp_resolution=0.01 --auto_timestamp_resolution=False --pos_thres=.15 --neg_thres=.15 --sigma_thres=0.03 --output_width=240 --output_height=180  --cutoff_hz=15 --disable_slomo --input_slowmotion_factor 1 --no_preview --truth [{},{},{}]".format(data, str(i),str(i),str(x),str(y),str(z))
-    string = "python v2e.py -i {}  --output_folder=output/ --dvs_h5 dvs{} --dvs_exposure duration 0.01 --overwrite --timestamp_resolution=0.01 --auto_timestamp_resolution=False --pos_thres=.15 --neg_thres=.15 --sigma_thres=0.03 --output_width=128 --output_height=128  --cutoff_hz=15 --disable_slomo --input_slowmotion_factor 1 --no_preview --label [{},{},{}] --dvs_aedat2 None --dvs_params clean".format(data, str(i),str(x),str(y),str(z))
+    string = "python v2e.py -i {}  --output_folder=output_vector/ --dvs_h5 dvs{} --dvs_exposure duration 0.01 --overwrite --timestamp_resolution=0.01 --auto_timestamp_resolution=False --pos_thres=.15 --neg_thres=.15 --sigma_thres=0.03 --output_width=128 --output_height=128  --cutoff_hz=15 --disable_slomo --input_slowmotion_factor 1 --no_preview --label [{},{},{}] --dvs_aedat2 None --dvs_params clean".format(data, str(i).zfill(5),str(x),str(y),str(z))
+    # string = "python v2e.py -i {}  --output_folder=output/ --dvs_h5 dvs{} --dvs_exposure duration 0.01 --overwrite --timestamp_resolution=0.01 --auto_timestamp_resolution=False --pos_thres=.15 --neg_thres=.15 --sigma_thres=0.03 --output_width=128 --output_height=128  --cutoff_hz=15 --disable_slomo --input_slowmotion_factor 1 --no_preview --label [{},{},{}] --dvs_aedat2 None --dvs_params clean".format(data, str(i),str(x),str(y),str(z))
     _ = os.system(string)
 
